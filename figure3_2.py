964e394f184f1961c491f31d71f1af2d4c2f74d2
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-04 00:00:24 -0500

    Create figure3_2.py

---
+++ figure3_2.py
@@ -0,0 +1,11 @@
+# Test if an int > 2 is prime. If not, print smallest divisor
+x = int(input('Enter an integer greater than 2: '))
+smallest_divisor = None
+for guess in range(2, x):
+	if x % guess == 0:
+		smallest_divisor = guess
+		break
+if smallest_divisor != None:
+	print(f'Smallest divisor of {x} is {smallest_divisor}')
+else:
+	print(f'{x} is a prime number.')
