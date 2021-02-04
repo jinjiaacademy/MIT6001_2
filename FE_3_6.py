c8d900a7265932936cbd467e1cfe6b2166c4f72a
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-04 13:05:44 -0500

    Create FE_3_6.py

---
+++ FE_3_6.py
@@ -0,0 +1,6 @@
+'''
+Add some code to the implementation of Newton-Raphson that keeps track of the number
+of iterations used to find the root. Use that code as part of a program that compares
+the efficiency of Newton-Raphson and bisection search. (You should discover that
+Newton-Raphson is far more efficient.)
+'''
