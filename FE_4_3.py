a8425cef0fc7b980ad5c90c396e32e450472d6fd
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-06 23:40:13 -0500

    Create FE_4_3.py

---
+++ FE_4_3.py
@@ -0,0 +1,22 @@
+'''
+Write a function to test is_in
+'''
+def is_in(str1, str2):
+	if str1 in str2 or str2 in str1:
+		return True
+	else:
+		return False
+
+def test_is_in(str1s, str2s):
+	for str1 in str1s:
+		for str2 in str2s:
+			result = is_in(str1, str2)
+			if result:
+				val = True
+			else:
+				val = False
+			print(f'{str1} - {str2} : {val}')
+
+str1s = ['hello', 'stop', 'jinjia']
+str2s = ['lo', 'sp', 'inj']
+test_is_in(str1s, str2s)
