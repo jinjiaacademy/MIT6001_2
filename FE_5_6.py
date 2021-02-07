02db50cd5284e355f1a6455c22659560f9abba97
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-06 23:40:36 -0500

    Delete FE_5_6.py

--- FE_5_6.py
+++
@@ -1,9 +0,0 @@
-'''
-Write a lambda expression that has two numeric parameters.
-If the second argument equals zero, it should return None.
-Otherwise it should return the value of dividing the first argument
-by the second argument. Hint: use a conditional expression
-'''
-div = lambda x, y : x / y if y != 0 else None
-print(div(5, 10))
-print(div(5, 0))
