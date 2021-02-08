d6a4630ad4f264ebabf3b7306c83844d5bcbc445
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 13:20:28 -0500

    Create FE_3_5.py

---
+++ finger_exercises/FE_3_5.py
@@ -0,0 +1,8 @@
+'''
+The Empire State Building is 102 stories high. A man wanted to know the highest floor
+from which he could drop an egg without the egg breaking. He proposed to drop an egg 
+from the top floor. If it broke, he would go down a floor, and try it again. He would 
+do this until the egg did not break. At worst, this method requireds 102 eggs. Implement
+a method that at worst uses seven eggs.
+'''
+ 
