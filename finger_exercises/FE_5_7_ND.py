920af129631daa6eb3f86c3755ba9471aaa25733
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 18:21:47 -0500

    Create FE_5_7_ND.py

---
+++ finger_exercises/FE_5_7_ND.py
@@ -0,0 +1,6 @@
+'''
+Using encoder and encrypt as models, implement the functions decoder
+and decrypt. Use them to decrypt the message
+22*13*33*137*59*11*23*11*1*57*6*13*1*2*6*57*2*6*1*22*13*33*137*59*11*23*11*1*57*6*173*7*11
+which was encrypted using the opening of Don Quixote.
+'''
