291dc3d68b1023529c1bd4c2b158c2dd6616d36f
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 13:19:31 -0500

    Delete name.py

--- name.py
+++
@@ -1,9 +0,0 @@
-def print_name(first_name, last_name, reverse):
-	if reverse:
-		print(last_name, first_name)
-	else:
-		print(first_name, last_name)
-
-print_name('jinjia', 'liu', True)
-print_name('jinjia', 'liu', False)
-print_name('jinjia', last_name='liu', reverse=True)
