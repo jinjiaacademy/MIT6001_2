95a04211c7c14cd026f8e4ac91080791bc957718
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 13:20:23 -0500

    Create FE_2_5.py

---
+++ finger_exercises/FE_2_5.py
@@ -0,0 +1,15 @@
+'''
+Write a program that prints the sum of the prime numbers 
+greater than 2 and less than 100.
+'''
+total = 0
+
+for number in range(3, 10):
+	for i in range (2, number):
+		if number % i == 0:
+			break
+	else:
+		print(number)
+		total += number
+
+print(f'Total: {total}')
