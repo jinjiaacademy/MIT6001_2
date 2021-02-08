4de65a359dc5d1aee5276828717b362ff66fe84c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 13:19:15 -0500

    Create FE_1_1.py

---
+++ finger_exercises/FE_1_1.py
@@ -0,0 +1,6 @@
+'''
+Computers can be annoyingly literal. If you don't tell them exactly what you want them to do,
+they are likely to do the wrong thing. Try writing an algorithm for driving between two destinations.
+Write it the way you would for a person, and then imagine what would happen if that person were
+as stupid as a computer, and executed the algorithm exactly as written.
+'''
