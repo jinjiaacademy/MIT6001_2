c8c637ad433fb38d8d3de883a742d3bf2b55323e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 15:16:08 -0500

    Update FE_5_1.py

--- finger_exercises/FE_5_1.py
+++ finger_exercises/FE_5_1.py
@@ -2,6 +2,5 @@
 Write an expression that evaluates to the mean of a tuple of numbers.
 Use the function sum()
 '''
-def meanTuple():
-	t1 = ()
-	return sum(t1)/len(t1)
+def meanTuple(t):
+	return sum(t)/len(t)
