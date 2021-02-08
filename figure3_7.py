326b0726055b8c8154b83b73b50d9845a98a8382
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 13:18:01 -0500

    Delete figure3_7.py

--- figure3_7.py
+++
@@ -1,8 +0,0 @@
-# Newton-Raphson for square root
-# Find x such that x**2 - 24 is within epsilon of 0
-k = int(input('Enter an integer to calculate its square root: '))
-epsilon = 0.01
-guess = k/2
-while abs(guess**2 - k) >= epsilon:
-	guess = guess - (((guess**2) - k)/(2 * guess))
-print('Square root of', k, 'is about', guess)
