40882e6fdb63fb82cefb644bcf5fb4d6226c5433
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-09 14:31:50 -0500

    Create thanksgiving.py

---
+++ figure_code/thanksgiving.py
@@ -0,0 +1,17 @@
+import calendar as cal
+
+def find_thanksgiving(year):
+	month = cal.monthcalendar(year, 11)
+	if month[0][cal.THURSDAY] != 0:
+		thanksgiving = month[3][cal.THURSDAY]
+	else:
+		thanksgiving = month[4][cal.THURSDAY]
+	return thanksgiving
+
+def main():
+	year = int(input('Enter a year: '))
+	thanksgiving_day = find_thanksgiving(year)
+	print(f'In the year of {year}, the US Thanksgiving was on Nov. {thanksgiving_day}')
+
+if __name__ == '__main__':
+	main()	
