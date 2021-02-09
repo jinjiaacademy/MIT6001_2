b558b285d29a974497e6bdbffba8af5e7a41d114
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 19:17:34 -0500

    Create figure6_1.py

---
+++ figure_code/figure6_1.py
@@ -0,0 +1,15 @@
+def fact_iter(n):
+	'''Assumes n an int > 0
+	Return n!'''
+	result = 1
+	for i in range(1, n + 1):
+		result *= i
+	return result
+
+def fact_rec(n):
+	'''Assumes n an int > 0
+	Returns n!'''
+	if n == 1:
+		return n
+	else:
+		return n*fact_rec(n-1)
