9b5c11d0857998a87456070e17bfc5c786f45d6f
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-08 22:23:19 -0500

    Create figure6_6.py

---
+++ figure_code/figure6_6.py
@@ -0,0 +1,18 @@
+def fib(x):
+	'''Assumes x an int >= 0
+	Returns Fibonacci of x'''
+	global num_fib_calls
+	num_fib_calls += 1
+	if x == 0 or x == 1:
+		return 1
+	else:
+		return fib(x-1) + fib(x-2)
+
+def test_fib(n):
+	for i in range(n+1):
+		global num_fib_calls
+		num_fib_calls = 0
+		print('fib of', i, '=', fib(i))
+		print('fib called', num_fib_calls, 'times')
+
+test_fib(6)
