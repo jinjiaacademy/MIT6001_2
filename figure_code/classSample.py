fa9b00403fc499c5ec644c2dae075891205198a8
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 15:35:04 -0500

    Update classSample.py

--- figure_code/classSample.py
+++ figure_code/classSample.py
@@ -1,20 +1,35 @@
 class Toy(object):
 	def __init__(self):
 		self._elems = []
+
 	def add(self, new_elems):
+		'''new_elems is a list'''
 		self._elems += new_elems
-	def size(self):
+
+	def __len__(self):
 		return len(self._elems)
 
-# print(type(Toy))
-# print(type(Toy.__init__), type(Toy.add), type(Toy.size))
+
+	def __add__(self, other):
+		new_toy = Toy()
+		new_toy._elems = self._elems + other._elems
+		return new_toy
+
+	def __eq__(self, other):
+		return self._elems == other._elems
+
+	def __str__(self):
+		return str(self._elems)
+
+	def __hash__(self):
+		return id(self)
 
 t1 = Toy()
-# print(type(t1))
-# print(type(t1.add))
 t2 = Toy()
-# print(t1 is t2)
-t1.add([3, 4])
-t2.add([4])
-print(t1.size() + t2.size())
-print(t1, t2)
+t1.add([1, 2])
+t2.add([3, 4])
+t3 = t1 + t2
+print('The value of t3 is', t3)
+print('The length of t3 is', len(t3))
+d = {t1: 'A', t2: 'B'}
+print('The value', d[t1], 'is associated with the key t1 in d.')
