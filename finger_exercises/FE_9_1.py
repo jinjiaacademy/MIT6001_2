b6cdb69d4f294accfd5dfb00dbc845f341ff46ab
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:55 -0500

    Update FE_9_1.py

--- finger_exercises/FE_9_1.py
+++ finger_exercises/FE_9_1.py
@@ -4,21 +4,21 @@ Use a try-except block. Hint: before starting to code, you
 might want to type something like 1 + 'a' in to the shell 
 to see what kind of exception is raised.
 '''
+
+
 def sum_digits(s):
-	'''Assumes s is a string
-	Returns the sum of the decimal digits in s
-	For example, if s is 'a2b3c' it returns 5'''
-	total = 0
-	try:
-		type(s) == str
-		for i in s:
-			if i in '1234567890':
-				total += int(i)
-		return total
-	except TypeError:
-		print('Unsuported type, please enter a string')
-	
-	
-	
+    '''Assumes s is a string
+    Returns the sum of the decimal digits in s
+    For example, if s is 'a2b3c' it returns 5'''
+    total = 0
+    try:
+        type(s) == str
+        for i in s:
+            if i in '1234567890':
+                total += int(i)
+        return total
+    except TypeError:
+        print('Unsuported type, please enter a string')
+
 
 print(sum_digits(123))
