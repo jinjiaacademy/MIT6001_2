351c40e96f76d5dd00e7bd1cb5b575fbff366863
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:08 -0500

    Update figure3_4.py

--- figure_code/figure3_4.py
+++ figure_code/figure3_4.py
@@ -4,10 +4,10 @@ step = epsilon**2
 num_guesses = 0
 ans = 0.0
 while abs(ans**2 - x) >= epsilon and ans*ans <= x:
-	ans += step
-	num_guesses += 1
+    ans += step
+    num_guesses += 1
 print('number of guesses =', num_guesses)
 if abs(ans**2 - x) >= epsilon:
-	print('Failed on square root of', x)
+    print('Failed on square root of', x)
 else:
-	print(ans, 'is close to square root of', x)
+    print(ans, 'is close to square root of', x)
