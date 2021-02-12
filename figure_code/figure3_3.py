9e4489c8ae69ef84ed1610160cc99be7b9a69cab
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:05 -0500

    Update figure3_3.py

--- figure_code/figure3_3.py
+++ figure_code/figure3_3.py
@@ -2,13 +2,13 @@
 x = int(input('Enter an integer greater than 2: '))
 smallest_divisor = None
 if x % 2 == 0:
-	smallest_divisor = 2
+    smallest_divisor = 2
 else:
-	for guess in range(3, x, 2):
-		if x % guess == 0:
-			smallest_divisor = guess
-			break
+    for guess in range(3, x, 2):
+        if x % guess == 0:
+            smallest_divisor = guess
+            break
 if smallest_divisor != None:
-	print('Smallest divisor of', x, 'is', smallest_divisor)
+    print('Smallest divisor of', x, 'is', smallest_divisor)
 else:
-	print(x, 'is a prime number.')
+    print(x, 'is a prime number.')
