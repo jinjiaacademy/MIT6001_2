3c5fc6e76f558e894a100e8b696df186c1874d9a
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 21:23:26 -0500

    Update figure6_4.py

--- figure_code/figure6_4.py
+++ figure_code/figure6_4.py
@@ -1,27 +1,28 @@
 def is_palindrome(s):
-	'''Assumes s is a str
-	Returns True if letters in s form a palindrome;
-	False otherwise. Non-letters and capitalization are ignored.'''
+    '''Assumes s is a str
+    Returns True if letters in s form a palindrome;
+    False otherwise. Non-letters and capitalization are ignored.'''
 
-	def to_char(s):
-		s = s.lower()
-		letters = ''
-		for c in s:
-			if c in 'abcdefghijklmnopqrstuvwxyz':
-				letters += c
-		return letters
+    def to_char(s):
+        s = s.lower()
+        letters = ''
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                letters += c
+        return letters
 
-	def is_pal(s):
-		print(' is_pal called with', s)
-		if len(s) <= 1:
-			print(' About to return True from base case')
-			return True
-		else:
-			answer = s[0] == s[-1] and is_pal(s[1:-1])
-			print(' About to return', answer, 'for', s)
-			return answer
+    def is_pal(s):
+        print(' is_pal called with', s)
+        if len(s) <= 1:
+            print(' About to return True from base case')
+            return True
+        else:
+            answer = s[0] == s[-1] and is_pal(s[1:-1])
+            print(' About to return', answer, 'for', s)
+            return answer
 
-	return is_pal(to_char(s))
+    return is_pal(to_char(s))
+
 
 print('Try dogGod')
 print(is_palindrome('dogGod'))
