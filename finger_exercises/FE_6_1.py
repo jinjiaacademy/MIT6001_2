4fb44d0ea1ddb20b26629cfa4075dd0bbafe6218
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:43 -0500

    Update FE_6_1.py

--- finger_exercises/FE_6_1.py
+++ finger_exercises/FE_6_1.py
@@ -4,10 +4,13 @@ usint the formula
 1 + 1/2 + ... + 1/n
 Write a recursive function that computes this.
 '''
+
+
 def harmonic_sum(n):
-	if n == 1:
-		return n
-	else:
-		return (1/n) + harmonic_sum(n-1)
+    if n == 1:
+        return n
+    else:
+        return (1/n) + harmonic_sum(n-1)
+
 
 print(harmonic_sum(5))
