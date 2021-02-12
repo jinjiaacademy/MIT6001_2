3610f63efc246791bc498006ece9aa13821a7583
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:58 -0500

    Update FE_3_3.py

--- finger_exercises/FE_3_3.py
+++ finger_exercises/FE_3_3.py
@@ -7,12 +7,12 @@ num_guesses, low = 0, 0
 high = max(1, x)
 ans = (high + low) / 2
 while abs(ans**2 - x) >= epsilon:
-	print('low =', low, 'high =', high, 'ans =', ans)
-	num_guesses += 1
-	if ans ** 2 < x:
-		low = ans
-	else:
-		high = ans
-	ans = (high + low) / 2
+    print('low =', low, 'high =', high, 'ans =', ans)
+    num_guesses += 1
+    if ans ** 2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low) / 2
 print('number of guesses =', num_guesses)
 print(ans, 'is close to square root of', x)
