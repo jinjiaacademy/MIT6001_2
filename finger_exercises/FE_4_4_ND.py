c379a1dbf561c2a5dc14a7222119066f89ef1ea3
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:13 -0500

    Update FE_4_4_ND.py

--- finger_exercises/FE_4_4_ND.py
+++ finger_exercises/FE_4_4_ND.py
@@ -4,11 +4,13 @@ as arguments. If called with two arguments, the function prints
 the product of the two arguments. If called with one argument,
 it prints that argument.
 '''
+
+
 def mult(x, y, single=True):
-	if single:
-		print(x)
-	else:
-		print(x * y)
+    if single:
+        print(x)
+    else:
+        print(x * y)
 
 
 mult(10, 20, False)
