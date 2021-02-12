21205dc6502f5e7dac50cb5c4f83f78b35c9696b
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:50 -0500

    Update FE_2_5.py

--- finger_exercises/FE_2_5.py
+++ finger_exercises/FE_2_5.py
@@ -5,11 +5,11 @@ greater than 2 and less than 100.
 total = 0
 
 for number in range(3, 10):
-	for i in range (2, number):
-		if number % i == 0:
-			break
-	else:
-		print(number)
-		total += number
+    for i in range(2, number):
+        if number % i == 0:
+            break
+    else:
+        print(number)
+        total += number
 
 print(f'Total: {total}')
