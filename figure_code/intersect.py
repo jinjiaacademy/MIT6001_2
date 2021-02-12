7f1fff36617fa887e6932307b45d07ccf5967fd9
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 21:23:50 -0500

    Update intersect.py

--- figure_code/intersect.py
+++ figure_code/intersect.py
@@ -1,10 +1,9 @@
 def intersect(t1, t2):
-	'''Assumes t1 and t2 are tuples
-	Returna a tuple containing elements that are in 
-	both t1 and t2'''
-	result = ()
-	for e in t1:
-		if e in t2:
-			result += (e,)
-	return result
-	
+    '''Assumes t1 and t2 are tuples
+    Returna a tuple containing elements that are in 
+    both t1 and t2'''
+    result = ()
+    for e in t1:
+        if e in t2:
+            result += (e,)
+    return result
