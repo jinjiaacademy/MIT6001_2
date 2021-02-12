8f867deb0d83dab3eeab29488a4a6ad581ff8a5e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:52 -0500

    Update FE_7_3.py

--- finger_exercises/FE_7_3.py
+++ finger_exercises/FE_7_3.py
@@ -4,17 +4,19 @@ Fibonnaci sequence to a file name fib_file. Each number should be
 on a seperate line in the file. The program should then read the
 numbers from the file and print them.
 '''
+
+
 def fib(n):
-	if n == 0 or n == 1:
-		return 1
-	else:
-		return fib(n-1) + fib(n-2)
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 
 with open('fib_file', 'w') as f:
-	for i in range(10):
-		f.write(f'{fib(i)}\n')
+    for i in range(10):
+        f.write(f'{fib(i)}\n')
 
 with open('fib_file', 'r') as f:
-	for line in f:
-		print(line[:-1])
+    for line in f:
+        print(line[:-1])
