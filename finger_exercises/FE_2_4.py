7e2d2151fc75a35f593796f0eded0f4f9561032e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:48 -0500

    Update FE_2_4.py

--- finger_exercises/FE_2_4.py
+++ finger_exercises/FE_2_4.py
@@ -7,12 +7,12 @@ num_input = 1
 num = int(input(f'Enter number {num_input}: '))
 
 while num_input < 10:
-	num_input += 1
-	num_new = int(input(f'Enter number {num_input}: '))
-	if num_new % 2 != 0 and num_new > num:
-		num = num_new
+    num_input += 1
+    num_new = int(input(f'Enter number {num_input}: '))
+    if num_new % 2 != 0 and num_new > num:
+        num = num_new
 
 if num % 2 == 0:
-	print(f'None of numbers are odd.')
+    print(f'None of numbers are odd.')
 else:
-	print(f'The largest odd number that was entered is {num}')
+    print(f'The largest odd number that was entered is {num}')
