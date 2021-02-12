3a5b342f818d9c7e6c7c406505a35a4a767e99ee
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:07:53 -0500

    Update FE_3_1.py

--- finger_exercises/FE_3_1.py
+++ finger_exercises/FE_3_1.py
@@ -6,10 +6,10 @@ Hint: if y * z = x and y is the smallest divisor of x, z is the largest divisor 
 x = int(input('Enter an integer greater than 2: '))
 smallest_divisor = None
 for guess in range(2, x):
-	if x % guess == 0:
-		smallest_divisor = guess
-		break
+    if x % guess == 0:
+        smallest_divisor = guess
+        break
 if smallest_divisor != None:
-	print(f'Largest divisor of {x} is {int(x/smallest_divisor)}')
+    print(f'Largest divisor of {x} is {int(x/smallest_divisor)}')
 else:
-	print(f'{x} is a prime number.')
+    print(f'{x} is a prime number.')
