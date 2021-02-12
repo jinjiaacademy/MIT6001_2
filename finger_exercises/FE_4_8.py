f9a33a96ec2c00e5ef0a74b624441aac4874e0d0
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:24 -0500

    Update FE_4_8.py

--- finger_exercises/FE_4_8.py
+++ finger_exercises/FE_4_8.py
@@ -1,13 +1,16 @@
 ''' 
 Use find to implement a function satisfying the specification
 '''
+
+
 def find_last(s, sub):
-	'''s and sub are non-empty strings,
-	Returns the index of the last occurrence of sub in s.
-	Returns None if sub does not occur in s'''
-	if s.find(sub) == -1:
-		return None
-	return s.rfind(sub)
+    '''s and sub are non-empty strings,
+    Returns the index of the last occurrence of sub in s.
+    Returns None if sub does not occur in s'''
+    if s.find(sub) == -1:
+        return None
+    return s.rfind(sub)
+
 
 print(find_last('jinjia', 'i'))
 print(find_last('jinjia', 'j'))
