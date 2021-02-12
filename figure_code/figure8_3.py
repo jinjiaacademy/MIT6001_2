22961905f70ae6491e14f5638776ac1f65d9e451
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 21:23:31 -0500

    Update figure8_3.py

--- figure_code/figure8_3.py
+++ figure_code/figure8_3.py
@@ -1,20 +1,21 @@
 def is_pal(x):
-	'''Assumes x is a list
-	Returns True if the list is a palindrome; False otherwise'''
-	temp = x[:]
-	temp.reverse()
-	return temp == x
+    '''Assumes x is a list
+    Returns True if the list is a palindrome; False otherwise'''
+    temp = x[:]
+    temp.reverse()
+    return temp == x
+
 
 def silly(n):
-	'''Assumes n is an int > 0
-	Gets n inputs from user
-	Prints 'Yes' if the sequence of inputs forms a palindrome;
-	'No' otherwise'''
-	result = []
-	for i in range(n):
-		elem = input('Enter elements: ')
-		result.append(elem)
-	if is_pal(result):
-		print('Yes')
-	else:
-		print('No')
+    '''Assumes n is an int > 0
+    Gets n inputs from user
+    Prints 'Yes' if the sequence of inputs forms a palindrome;
+    'No' otherwise'''
+    result = []
+    for i in range(n):
+        elem = input('Enter elements: ')
+        result.append(elem)
+    if is_pal(result):
+        print('Yes')
+    else:
+        print('No')
