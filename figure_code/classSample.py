6906af7d4398124be9f27f2441623cdd2c658444
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 20:47:08 -0500

    Update classSample.py

--- figure_code/classSample.py
+++ figure_code/classSample.py
@@ -1,28 +1,28 @@
 class Toy(object):
-	def __init__(self):
-		self._elems = []
+    def __init__(self):
+        self._elems = []
 
-	def add(self, new_elems):
-		'''new_elems is a list'''
-		self._elems += new_elems
+    def add(self, new_elems):
+        '''new_elems is a list'''
+        self._elems += new_elems
 
-	def __len__(self):
-		return len(self._elems)
+    def __len__(self):
+        return len(self._elems)
 
+    def __add__(self, other):
+        new_toy = Toy()
+        new_toy._elems = self._elems + other._elems
+        return new_toy
 
-	def __add__(self, other):
-		new_toy = Toy()
-		new_toy._elems = self._elems + other._elems
-		return new_toy
+    def __eq__(self, other):
+        return self._elems == other._elems
 
-	def __eq__(self, other):
-		return self._elems == other._elems
+    def __str__(self):
+        return str(self._elems)
 
-	def __str__(self):
-		return str(self._elems)
+    def __hash__(self):
+        return id(self)
 
-	def __hash__(self):
-		return id(self)
 
 t1 = Toy()
 t2 = Toy()
