565cb55155e5fb2be81c72326016303a26102c8a
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 21:22:54 -0500

    Update figure4_3.py

--- figure_code/figure4_3.py
+++ figure_code/figure4_3.py
@@ -1,15 +1,15 @@
 def find_root(x, power, epsilon):
-	# Find interval containing answer:
-	if x < 0 and power%2 == 0:
-		return None # Negative number has no even-powered roots
-	low = min(-1, x)
-	high = max(1, x)
-	# Use bisection search
-	ans = (high + low) / 2
-	while abs(ans**power - x) >= epsilon:
-		if ans**power < x:
-			low = ans
-		else:
-			high = ans
-		ans = (high + low) / 2
-	return ans
+    # Find interval containing answer:
+    if x < 0 and power % 2 == 0:
+        return None  # Negative number has no even-powered roots
+    low = min(-1, x)
+    high = max(1, x)
+    # Use bisection search
+    ans = (high + low) / 2
+    while abs(ans**power - x) >= epsilon:
+        if ans**power < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low) / 2
+    return ans
