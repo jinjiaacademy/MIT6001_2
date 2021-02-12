a842d1a08e7888cdf4af353d91e38b8fde13634e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-11 22:08:19 -0500

    Update FE_4_6.py

--- finger_exercises/FE_4_6.py
+++ finger_exercises/FE_4_6.py
@@ -4,6 +4,8 @@ If the second argument equals zero, it should return None.
 Otherwise it should return the value of dividing the first argument
 by the second argument. Hint: use a conditional expression
 '''
-div = lambda x, y : x / y if y != 0 else None
+def div(x, y): return x / y if y != 0 else None
+
+
 print(div(5, 10))
 print(div(5, 0))
