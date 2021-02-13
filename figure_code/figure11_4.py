24403f343dd4ab28114647fa46f6910075a2bddd
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-12 21:18:34 -0500

    Create figure11_4.py

---
+++ figure_code/figure11_4.py
@@ -0,0 +1,13 @@
+def is_subset(L1, L2):
+    '''Assumes L1 and L2 are lists.
+    Returns True if each element in L1 is also in L2
+    and False otherwise'''
+    for e1 in L1:
+        matched = False
+        for e2 in L2:
+            if e1 == e2:
+                matched = True
+                break
+        if not matched:
+            return False
+    return False
