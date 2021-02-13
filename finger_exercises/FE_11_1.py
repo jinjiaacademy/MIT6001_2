940bca031e401728dc8aac62d6bd133e3457554d
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-12 20:48:29 -0500

    Create FE_11_1.py

---
+++ finger_exercises/FE_11_1.py
@@ -0,0 +1,22 @@
+'''
+What is the asymptotic complexity of each of the following functions?
+'''
+def g(L, e):
+    '''L a list of ints, e is an int'''
+    for i in range(100):
+        for e1 in L:
+            if e1 == e:
+                return True
+    return False
+
+'''Answer: 100 * len(L)'''
+
+def h(L, e):
+    '''L a list of ints, e is an int'''
+    for i in range(e):
+        for e1 in L:
+            if e1 == e:
+                return True
+    return False
+
+'''Answer: e * len(L)
