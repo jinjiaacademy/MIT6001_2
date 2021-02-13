69c70f3d304cfbcadf0a80eaf24bed67afa72e79
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-12 20:05:54 -0500

    Update func.py

--- figure_code/func.py
+++ figure_code/func.py
@@ -12,3 +12,13 @@ def linear_search(L, x):
         if e == x:
             return True
     return False
+
+
+def fact(n):
+    '''Assumes n is a positive int
+    Returns n!'''
+    answer = 1
+    while n > 1:
+        answer *= n
+        n -= 1
+    return answer
