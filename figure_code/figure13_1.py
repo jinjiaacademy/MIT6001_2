38ce75848caaef3ca13f2f78698f7ed4aa187473
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-17 15:06:17 -0500

    Create figure13_1.py

---
+++ figure_code/figure13_1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4], [1, 7, 3, 5])
+plt.figure(1)
+plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
+plt.figure(2)
+plt.plot([1, 4, 2, 3], [5, 6, 7, 8])
+plt.savefig('Figure-Addie')
+plt.figure(1)
+plt.plot([5, 6, 10, 3])
+plt.savefig('Figure-Jane')
+plt.show()
