e3e7cc533ea63970684787618308d66a627409aa
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-02-18 15:14:13 -0500

    Create FE_12_1.py

---
+++ finger_exercises/FE_12_1.py
@@ -0,0 +1,29 @@
+'''
+Why does the code use mid+1 rather than mid in the second recursive call
+'''
+
+
+def search(L, e):
+    '''Assumes L is a list, the elements of which are in
+    ascending order.
+    Returns True if e is in L and False otherwise.'''
+
+    def bin_search(L, e, low, high):
+        # Decrements high - low
+        if high == low:
+            return L[low] == e
+        mid = (high + low) // 2
+        if L[mid] == e:
+            return True
+        elif L[mid] > e:
+            if low == mid:
+                return False
+            else:
+                return bin_search(L, e, low, mid-1)
+        else:
+            return bin_search(L, e, mid+1, high)
+
+    if len(L) == 0:
+        return False
+    else:
+        return bin_search(L, e, 0, len(L)-1)
