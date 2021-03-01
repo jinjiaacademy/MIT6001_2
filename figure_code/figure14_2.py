18ef51e756ebc05d28b09580a844082e9d7fbb79
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-01 16:01:05 -0500

    Update figure14_2.py

--- figure_code/figure14_2.py
+++ figure_code/figure14_2.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# # Figure 14-2 from page 284
 class Item(object):
     def __init__(self, n, v, w):
         self._name = n
@@ -28,10 +31,12 @@ def weight_inverse(item):
 def density(item):
     return item.get_value()/item.get_weight()
 
+# # Figure 14-3 from page 284
+
 
 def greedy(items, max_weight, key_function):
-    '''Assumes items a list, max_weight >= 0,
-    key_function maps elements of items to numbers'''
+    """Assumes items a list, max_weight >= 0,
+       key_function maps elements of items to numbers"""
     items_copy = sorted(items, key=key_function, reverse=True)
     result = []
     total_value, total_weight = 0.0, 0.0
@@ -41,6 +46,8 @@ def greedy(items, max_weight, key_function):
             total_weight += items_copy[i].get_weight()
             total_value += items_copy[i].get_value()
     return (result, total_value)
+
+# # Figure 14-4 from page 286
 
 
 def build_items():
@@ -57,17 +64,82 @@ def test_greedy(items, max_weight, key_function):
     taken, val = greedy(items, max_weight, key_function)
     print('Total value of items taken is', val)
     for item in taken:
-        print('     ', item)
+        print('   ', item)
 
 
 def test_greedys(max_weight=20):
     items = build_items()
     print('Use greedy by value to fill knapsack of size', max_weight)
     test_greedy(items, max_weight, value)
-    print('\nUse greedy by weight to fill knapsack of size', max_weight)
+    print('\nUse greedy by weight to fill knapsack of size',
+          max_weight)
     test_greedy(items, max_weight, weight_inverse)
-    print('\nUse greedy by density to fill knapsack of size', max_weight)
+    print('\nUse greedy by density to fill knapsack of size',
+          max_weight)
     test_greedy(items, max_weight, density)
 
+# # Code from page 287
+# test_greedys()
 
-test_greedys(max_weight=20)
+# # Code from Figure 11-6
+
+
+def get_binary_rep(n, num_digits):
+    """Assumes n and numDigits are non-negative ints
+       Returns a str of length numDigits that is a binary
+       representation of n"""
+    result = ''
+    while n > 0:
+        result = str(n % 2) + result
+        n = n//2
+    if len(result) > num_digits:
+        raise ValueError('not enough digits')
+    for i in range(num_digits - len(result)):
+        result = '0' + result
+    return result
+
+
+def gen_powerset(L):
+    """Assumes L is a list
+       Returns a list of lists that contains all possible
+       combinations of the elements of L. E.g., if
+       L is [1, 2] it will return a list with elements
+       [], [1], [2], and [1,2]."""
+    powerset = []
+    for i in range(0, 2**len(L)):
+        bin_str = get_binary_rep(i, len(L))
+        subset = []
+        for j in range(len(L)):
+            if bin_str[j] == '1':
+                subset.append(L[j])
+        powerset.append(subset)
+    return powerset
+
+# # Figure 14-5 on page 289
+
+
+def choose_best(pset, max_weight, get_val, get_weight):
+    best_val = 0.0
+    best_set = None
+    for items in pset:
+        items_val = 0.0
+        items_weight = 0.0
+        for item in items:
+            items_val += get_val(item)
+            items_weight += get_weight(item)
+        if items_weight <= max_weight and items_val > best_val:
+            best_val = items_val
+            best_set = items
+    return (best_set, best_val)
+
+
+def test_best(max_weight=20):
+    items = build_items()
+    pset = gen_powerset(items)
+    taken, val = choose_best(pset, max_weight, Item.get_value,
+                             Item.get_weight)
+    print('Total value of items taken is', val)
+    for item in taken:
+        print(item)
+
+test_best()
