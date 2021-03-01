5f1c74f6cbaa431b84b09e865ac73428e6fb6a56
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-01 15:38:34 -0500

    Create figure14_4.py

---
+++ figure_code/figure14_4.py
@@ -0,0 +1,25 @@
+def build_items():
+    names = ['clock', 'painting', 'radio', 'vase', 'book', 'computer']
+    values = [175, 90, 20, 50, 10, 200]
+    weights = [10, 9, 4, 2, 1, 20]
+    Items = []
+    for i in range(len(values)):
+        Items.append(Item(names[i], values[i], weights[i]))
+    return Items
+
+
+def test_greedy(items, max_weight, key_function):
+    taken, val = greedy(items, max_weight, key_function)
+    print('Total value of items taken is', val)
+    for item in taken:
+        print('     ', item)
+
+
+def test_greedys(max_weight=20):
+    items = build_items()
+    print('Use greedy by value to fill knapsack of size', max_weight)
+    test_greedy(items, max_weight, value)
+    print('\nUse greedy by weight to fill knapsack of size', max_weight)
+    test_greedy(items, max_weight, weight_inverse)
+    print('\nUse greedy by density to fill knapsack of size', max_weight)
+    test_greedy(items, max_weight, density)
