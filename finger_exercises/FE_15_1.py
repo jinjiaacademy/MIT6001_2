641923185f1804dc09e55f24651c6777237fdace
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-02 15:37:11 -0500

    Create FE_15_1.py

---
+++ finger_exercises/FE_15_1.py
@@ -0,0 +1,9 @@
+'''
+Use the tabular method to implement a dynamic programming
+solution that meets the specification'''
+def make_change(coin_vals, change):
+    '''coin_vals is a list of positive ints and coin_vals[0] = 1
+    change is a positive int,
+    return the minimum number of coins needed to have a set of
+    coins the values of which sum to change. Coins may be used more
+    than once. For example, make_change([1, 5, 8], 11) should return 3'''
