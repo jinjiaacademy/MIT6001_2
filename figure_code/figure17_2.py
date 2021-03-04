e877f798f110423b9a849b9fdcae7c0d667f8d48
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-04 13:50:06 -0500

    Update figure17_2.py

--- figure_code/figure17_2.py
+++ figure_code/figure17_2.py
@@ -1,4 +1,7 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate
 
 
 def flip(num_flips):
@@ -18,4 +21,65 @@ def flip_sim(num_flips_per_trial, num_trials):
     mean = sum(frac_heads)/len(frac_heads)
     return mean
 
-print('Mean =', flip_sim(100, 10000))
+
+def regress_to_mean(num_flips, num_trials):
+    # Get fraction of heads for each trial of num_flips
+    frac_heads = []
+    for t in range(num_trials):
+        frac_heads.append(flip(num_flips))
+    # Find trials with extreme results and for each the next trial
+    extremes, next_trials = [], []
+    for i in range(len(frac_heads) - 1):
+        if frac_heads[i] < 0.33 or frac_heads[i] > 0.66:
+            extremes.append(frac_heads[i])
+            next_trials.append(frac_heads[i+1])
+    # Plot results
+    plt.plot(range(len(extremes)), extremes, 'ko',
+             label='Extreme')
+    plt.plot(range(len(next_trials)), next_trials, 'k^',
+             label='Next Trial')
+    plt.axhline(0.5)
+    plt.ylim(0, 1)
+    plt.xlim(-1, len(extremes) + 1)
+    plt.xlabel('Extreme Example and Next Trial')
+    plt.ylabel('Fraction Heads')
+    plt.title('Regression to the Mean')
+    plt.legend(loc='best')
+    plt.show()
+
+
+def flip_plot(min_exp, max_exp):
+    '''Assumes min_exp and max_exp positive ints; min_exp < max_exp
+    Plots results of 2**min_exp to 2**max_exp coin flips'''
+    ratios, diffs, xAxis = [], [], []
+    for exp in range(min_exp, max_exp + 1):
+        xAxis.append(2**exp)
+    for num_flips in xAxis:
+        num_heads = 0
+        for n in range(num_flips):
+            if random.choice(('H', 'T')) == 'H':
+                num_heads += 1
+        num_tails = num_flips - num_heads
+        try:
+            ratios.append(num_heads/num_tails)
+            diffs.append(abs(num_heads-num_tails))
+        except ZeroDivisionError:
+            continue
+    plt.title('Difference Between Heads and Tails')
+    plt.xlabel('Number of Flips')
+    plt.ylabel('Abs(#Heads - #Tails)')
+    plt.xticks(rotation='vertical')
+    plt.plot(xAxis, diffs, 'ko')
+    plt.figure()
+    plt.title('Heads/Tails Ratios')
+    plt.xlabel('Number of Flips')
+    plt.ylabel('#Heads / #Tails')
+    plt.xticks(rotation='vertical')
+    plt.plot(xAxis, ratios, 'ko')
+    plt.show()
+
+
+# print('Mean =', flip_sim(100, 10000))
+# regress_to_mean(15, 50)
+random.seed(0)
+flip_plot(4, 20)
