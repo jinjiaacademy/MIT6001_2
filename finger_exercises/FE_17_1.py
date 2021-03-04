b9136b9edb767ce562eb98014691ed98879d3d52
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-04 13:50:16 -0500

    Create FE_17_1.py

---
+++ finger_exercises/FE_17_1.py
@@ -0,0 +1,7 @@
+'''
+Andrea averages 5 strokes a hole when she plays golf. One
+day, she took 40 strokes to complete the first nine holes.
+Her partner conjectured that she would probably regress to the
+mean and take 50 strokes to complete the next holes. Do you
+agree with her partner?
+'''
