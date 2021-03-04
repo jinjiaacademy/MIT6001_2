1b600907abb2a5699b5ec607faa3750ebdaaaadb
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-04 08:06:37 -0500

    Update figure16_2.py

--- figure_code/figure16_2.py
+++ figure_code/figure16_2.py
@@ -164,7 +164,64 @@ def sim_all_plot(drunk_kinds, walk_lengths, num_trials):
     plt.show()
 
 
+def get_final_locs(num_steps, num_trials, d_class):
+    locs = []
+    d = d_class()
+    for t in range(num_trials):
+        f = Field()
+        f.add_drunk(d, Location(0, 0))
+        for s in range(num_steps):
+            f.move_drunk(d)
+        locs.append(f.get_loc(d))
+    return locs
+
+
+def plot_locs(drunk_kinds, num_steps, num_trials):
+    style_choice = style_iterator(('k+', 'r^', 'mo'))
+    for d_class in drunk_kinds:
+        locs = get_final_locs(num_steps, num_trials, d_class)
+        x_vals, y_vals = [], []
+        for loc in locs:
+            x_vals.append(loc.get_x())
+            y_vals.append(loc.get_y())
+        meanX = sum(x_vals)/len(x_vals)
+        meanY = sum(x_vals)/len(y_vals)
+        cur_style = style_choice.next_style()
+        plt.plot(x_vals, y_vals, cur_style,
+                 label=(f'{d_class.__name__} mean loc. = <' +
+                        f'{meanX}, {meanY}>'))
+    plt.title(f'Location at End of Walks ({num_steps} steps)')
+    plt.xlabel('Steps East/West of Origin')
+    plt.ylabel('Steps North/South of Origin')
+    plt.legend(loc='best')
+    plt.show()
+
+def trace_walk(drunk_kinds, num_steps):
+    style_choice = style_iterator(('k+', 'r^', 'mo'))
+    f = Field()
+    for d_class in drunk_kinds:
+        d = d_class()
+        f.add_drunk(d, Location(0, 0))
+        locs = []
+        for s in range(num_steps):
+            f.move_drunk(d)
+            locs.append(f.get_loc(d))
+        x_vals, y_vals = [], []
+        for loc in locs:
+            x_vals.append(loc.get_x())
+            y_vals.append(loc.get_y())
+        cur_style = style_choice.next_style()
+        plt.plot(x_vals, y_vals, cur_style,
+                 label=d_class.__name__)
+    plt.title('Spots Visited on Walk ('
+              + str(num_steps) + ' steps)')
+    plt.xlabel('Steps East/West of Origin')
+    plt.ylabel('Steps North/South of Origin')
+    plt.legend(loc='best')
+    plt.show()
+
 # drunk_test((10, 100, 1000, 10000), 100, Usual_drunk)
 # sim_all((Usual_drunk, Cold_drunk, EW_drunk), (100, 1000), 10)
-sim_all_plot((Usual_drunk, Cold_drunk, EW_drunk),
-             (10, 100, 1000, 10000, 100000), 100)
+# sim_all_plot((Usual_drunk, Cold_drunk, EW_drunk),
+#              (10, 100, 1000, 10000, 100000), 100)
+plot_locs((Usual_drunk, Cold_drunk, EW_drunk), 100, 200)
