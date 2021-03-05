5198635389d24c582538d4bf5b518e5be8aa9f21
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-05 08:01:29 -0500

    Create FE_17_3.py

---
+++ finger_exercises/FE_17_3.py
@@ -0,0 +1,5 @@
+'''
+In figure17_19, why are the bins near the middle of the histogram
+taller than the bins near the sides? Hint: think about why 7 is the
+most common outcome of rolling a pair of dice.
+'''
