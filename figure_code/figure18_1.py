d8b62202b53ee2a3c344f29eb002e369cc695e62
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-06 14:43:10 -0500

    Update figure18_1.py

--- figure_code/figure18_1.py
+++ figure_code/figure18_1.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def roll_die():
-    return random.choice([1, 2, 3, 4, 5, 6])
+    # return random.choice([1, 2, 3, 4, 5, 6])
+    return random.choice([1, 1, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6])
 
 
 def check_pascal(num_trials):
@@ -36,37 +37,54 @@ class Craps_game(object):
                 self.dp_pushes += 1
             else:
                 self.dp_wins += 1
-            else:
-                point = throw
-                while True:
-                    throw = roll_die() + roll_die()
-                    if throw == point:
-                        self.pass_wins += 1
-                        self.dp_losses += 1
-                        break
-                    elif throw == 7:
-                        self.pass_losses += 1
-                        self.dp_wins += 1
-                        break
+        else:
+            point = throw
+            while True:
+                throw = roll_die() + roll_die()
+                if throw == point:
+                    self.pass_wins += 1
+                    self.dp_losses += 1
+                    break
+                elif throw == 7:
+                    self.pass_losses += 1
+                    self.dp_wins += 1
+                    break
 
     def pass_results(self):
         return (self.pass_wins, self.pass_losses)
+
     def dp_results(self):
         return (self.dp_wins, self.dp_losses, self.dp_pushes)
 
 
+def craps_sim(hands_per_game, num_games):
+    '''Assumes hands_per_game and num_games are ints > 0
+    Play num_games games of hands_per_game hands; print results'''
+    games = []
 
+    # Play num_games games
+    for t in range(num_games):
+        c = Craps_game()
+        for i in range(hands_per_game):
+            c.play_hand()
+        games.append(c)
 
+    # Produce statistics for each game
+    p_ROI_per_game, dp_ROI_per_game = [], []
+    for g in games:
+        wins, losses = g.pass_results()
+        p_ROI_per_game.append((wins-losses)/float(hands_per_game))
+        wins, losses, pushes = g.dp_results()
+        dp_ROI_per_game.append((wins-losses)/float(hands_per_game))
 
-
-
-
-
-
-
-
-
-
+    # Produce and print summary statistics
+    mean_ROI = str(round((100*sum(p_ROI_per_game)/num_games), 4)) + '%'
+    sigma = str(round(100*np.std(p_ROI_per_game), 4)) + '%'
+    print('Pass:', 'Mean ROI =', mean_ROI, 'Std. Dev. =', sigma)
+    mean_ROI = str(round((100*sum(dp_ROI_per_game)/num_games), 4)) + '%'
+    sigma = str(round(100*np.std(dp_ROI_per_game), 4)) + '%'
+    print('Don\'t pass:', 'Mean ROI =', mean_ROI, 'Std Dev =', sigma)
 
 
 # check_pascal(1000000)
+craps_sim(1000000, 10)
