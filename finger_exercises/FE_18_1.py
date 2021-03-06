9304299ca0a58745eac43288d774246f00f2de06
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-06 14:43:15 -0500

    Create FE_18_1.py

---
+++ finger_exercises/FE_18_1.py
@@ -0,0 +1,6 @@
+'''
+A 'big 6' bet pays even money if a 6 is rolled before a 7.
+Assuming 30 $5 bets per hour, write a Monte Carlo simulation
+that estimates the cost per hour and the standard deviation of
+that cost of playing 'big 6' bets
+'''
