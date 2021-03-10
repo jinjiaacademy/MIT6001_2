f0fa210db6664046bba1b8d65c773942617397a9
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-10 17:43:09 -0500

    Create FE_20_2.py

---
+++ finger_exercises/FE_20_2.py
@@ -0,0 +1,13 @@
+'''
+Finger exercise: In a vacuum, the speed of a falling object is defined
+by the equation v = v0 + gt, where v0 is the initial velocity of the object,
+t is the number of seconds the object has been falling, and g is the
+gravitational constant, roughly 9.8 m/sec2 on the surface of the Earth
+and 3.711 m/ sec2 on Mars. A scientist measures the velocity of a falling
+object on an unknown planet. She does this by measuring the downward
+velocity of an object at different points in time. At time 0, the object
+has an unknown velocity of v0. Implement a function that fits a model to
+the time and velocity data and estimates g for that planet and v0 for the
+experiment. It should return its estimates for g and v0, and also r-squared
+for the model.
+'''
