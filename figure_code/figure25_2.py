2c638ee1ddf0b86e4b5d66d107c2f26dd4b49a42
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-15 16:37:59 -0400

    Update figure25_2.py

--- figure_code/figure25_2.py
+++ figure_code/figure25_2.py
@@ -202,4 +202,26 @@ def contrived_test(num_trials, k, verbose=False):
         print('', c)
 
 
-contrived_test(1, 2, True)
+def contrived_test2(num_trials, k, verbose=False):
+    x_mean = 3
+    x_sd = 1
+    y_mean = 5
+    y_sd = 1
+    n = 8
+    d1_samples = gen_distribution(x_mean, x_sd, y_mean, y_sd, n, 'A')
+    plot_samples(d1_samples, 'k^')
+    d2_samples = gen_distribution(x_mean+3, x_sd, y_mean, y_sd, n, 'B')
+    plot_samples(d2_samples, 'ko')
+    d3_samples = gen_distribution(x_mean, x_sd, y_mean+3, y_sd, n, 'C')
+    plot_samples(d3_samples, 'kx')
+    clusters = try_k_means(d1_samples + d2_samples + d3_samples,
+                           k, num_trials, verbose)
+    plt.ylim(0, 12)
+    print('Final result has dissimilarity',
+          round(dissimilarity(clusters), 3))
+    for c in clusters:
+        print('', c)
+
+
+# contrived_test(50, 2, False)
+contrived_test2(40, 6)
