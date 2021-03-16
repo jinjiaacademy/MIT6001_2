f0f8cd8b808bdf4cc88ab619a03674e6ab294538
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-03-15 21:13:56 -0400

    Create figure26_6.py

---
+++ figure_code/figure26_6.py
@@ -0,0 +1,45 @@
+class Runner(object):
+    def __init__(self, name, gender, age, time):
+        self._name = name
+        self._feature_vec = np.array([age, time])
+        self._label = gender
+
+    def feature_dist(self, other):
+        return ((self._feature_vec-other._feature_vec)**2).sum()**0.5
+
+    def get_time(self):
+        return self._feature_vec[1]
+
+    def get_age(self):
+        return self._feature_vec[0]
+
+    def get_label(self):
+        return self._label
+
+    def get_features(self):
+        return self._feature_vec
+
+    def __str__(self):
+        return (f'{self._name}: {self.get_age()}, ' +
+                f'{self.get_time()}, {self._label}')
+
+
+def build_marathon_examples(file_name):
+    df = pd.read_csv(file_name)
+    examples = []
+    for index, row in df.iterrows():
+        a = Runner(row['Name'], row['Gender'], row['Age'], row['Time'])
+        examples.append(a)
+    return examples
+
+
+def divide_80_20(examples):
+    sample_indices = random.sample(range(len(examples)),
+                                   len(examples)//5)
+    training_set, test_set = [], []
+    for i in range(len(examples)):
+        if i in sample_indices:
+            test_set.append(examples[i])
+        else:
+            training_set.append(examples[i])
+    return training_set, test_set
