d11938b51e0843a0bdc66cd9733e7d508a9d8ad8
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-04-28 23:08:29 -0400

    Update figure9_1.py

--- figure_code/figure9_1.py
+++ figure_code/figure9_1.py
@@ -2,9 +2,11 @@ def get_ratio(vect1, vect2):
     '''Assumes: vect1 and vect2 are equal length lists of numbers
     Returns: a list containing the meaningful values of
             vect1[i]/vect2[i]'''
+
     ratio = []
     for index in range(len(vect1)):
         try:
+
             ratio.append(vect1[index]/vect2[index])
         except ZeroDivisionError:
             ratio.append(float('nan'))  # nan = not a number
